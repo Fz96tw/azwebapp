894ca23733cad1973f8ed198a0729c9ed5c948a2
Nadeem Haider <nadeemh@Nadeems-MBP.attlocal.net>
2023-09-24 17:02:30 -0400

    added coder endpoint

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ var t = time.Now()
 func main() {
 	http.HandleFunc("/", HelloServer)
 	http.HandleFunc("/version", VersionServer)
+	http.HandleFunc("/coder", CoderServer)
 	http.ListenAndServe(":80", nil)
 }
 func HelloServer(w http.ResponseWriter, r *http.Request) {
@@ -19,4 +20,8 @@ func HelloServer(w http.ResponseWriter, r *http.Request) {
 
 func VersionServer(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "service version %s", t)
+}
+
+func CoderServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Coded by NH")
 }
