c22978d1d825836d46207441c9a8f2c4529113de
Nadeem Haider <nadeemh@Nadeems-MBP.attlocal.net>
2023-09-24 16:32:52 -0400

    added new endpoint for version info

--- main.go
+++ main.go
@@ -3,12 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
+
+var t = time.Now()
 
 func main() {
 	http.HandleFunc("/", HelloServer)
+	http.HandleFunc("/version", VersionServer)
 	http.ListenAndServe(":80", nil)
 }
 func HelloServer(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+}
+
+func VersionServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "service version %s", t)
 }
